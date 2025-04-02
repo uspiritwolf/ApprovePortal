c793136e18c2b67132f3067488aab11e44ccfb95
Oleksii Chernykh <7448849+uspiritwolf@users.noreply.github.com>
2025-04-02 04:27:40 +0300

    Impl login page

--- approveportal.client/src/App.tsx
+++ approveportal.client/src/App.tsx
@@ -1,116 +1,17 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from "@/pages/login"
+import HomePage from "@/pages/home"
 
-interface Forecast {
-	date: string;
-	temperatureC: number;
-	temperatureF: number;
-	summary: string;
-}
-
-function WeatherPage(props: { token: string }) {
-	const [forecasts, setForecasts] = useState<Forecast[]>();
-
-	useEffect(() => {
-		populateWeatherData(props.token);
-	}, [props.token]);
-
-	const contents = forecasts === undefined
-		? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-		: <table className="table table-striped" aria-labelledby="tableLabel">
-			<thead>
-				<tr>
-					<th>Date</th>
-					<th>Temp. (C)</th>
-					<th>Temp. (F)</th>
-					<th>Summary</th>
-				</tr>
-			</thead>
-			<tbody>
-				{forecasts.map(forecast =>
-					<tr key={forecast.date}>
-						<td>{forecast.date}</td>
-						<td>{forecast.temperatureC}</td>
-						<td>{forecast.temperatureF}</td>
-						<td>{forecast.summary}</td>
-					</tr>
-				)}
-			</tbody>
-		</table>;
-
+function App()
+{
 	return (
-		<div>
-			<h1 id="tableLabel">Weather forecast</h1>
-			<p>This component demonstrates fetching data from the server.</p>
-			{contents}
-		</div>
+		<Router>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/login" element={<LoginPage />} />
+			</Routes>
+		</Router>
 	);
-
-	async function populateWeatherData(token: string) {
-		const response = await fetch('weatherforecast', {
-			headers: {
-				'Authorization': `Bearer ${token}`
-			}
-		});
-		if (response.ok) {
-			const data = await response.json();
-			setForecasts(data);
-		}
-	}
-}
-
-function LoginPage(props: { callback: (token: string) => void }) {
-	const [username, setUsername] = useState('');
-	const [password, setPassword] = useState('');
-	return (
-		<div>
-			<h1>Login</h1>
-			<form onSubmit={handleSubmit}>
-				<div>
-					<label>Username: </label>
-					<input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-				</div>
-				<div>
-					<label>Password: </label>
-					<input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-				</div>
-				<div>
-					<button type="submit">Login</button>
-				</div>
-			</form>
-		</div>
-	);
-	async function handleSubmit(e: React.FormEvent) {
-		e.preventDefault();
-		const response = await fetch('auth/login', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				Username: username,
-				Password: password
-			})
-		});
-		if (response.ok) {
-			const data = await response.json();
-			props.callback(data.token);
-		}
-	}
-}
-
-function App() {
-	const [token, setToken] = useState<string | null>(null);
-
-	function handleLogin(token: string) {
-		setToken(token);
-	}
-
-	if (token == null) {
-		return (<LoginPage callback={handleLogin} />)
-	} else {
-		return (<WeatherPage token={token} />)
-	}
 }
 
 export default App;
---
+++ approveportal.client/src/components/login-form.tsx
@@ -0,0 +1,114 @@
+import { useContext, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+import {
+	Alert,
+	AlertDescription,
+	AlertTitle,
+} from "@/components/ui/alert"
+
+import { AuthContext } from "@/context/AuthContext"
+
+interface LoginFormProps extends React.HTMLAttributes<HTMLDivElement>
+{
+	className?: string;
+}
+
+export function LoginForm({ className, ...props }: LoginFormProps)
+{
+	const navigate = useNavigate();
+	const authContext = useContext(AuthContext);
+	const [error, setError] = useState<string | null>(null);
+
+	async function sumbit(formData: FormData) {
+		const request = {
+			username: formData.get("username"),
+			password: formData.get("password"),
+		}
+
+		const response = await fetch("api/auth/login", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(request),
+		})
+		if (response.ok)
+		{
+			setError('')
+			authContext?.onLogin((await response.json()).token)
+				.then(() => navigate('/'))
+				.catch(() => setError('Failed to login'))
+		}
+		else
+		{
+			const data = await response.json()
+			let errorStr = data.title
+			for (const key in data.errors) {
+				errorStr += data.errors[key] + ' ';
+			}
+			setError(errorStr)
+			return
+		}
+	}
+
+	return (
+		<div className={cn("flex flex-col gap-6", className)} {...props}>
+			<Card>
+				<CardHeader>
+					<CardTitle className="text-2xl">Login</CardTitle>
+					<CardDescription>
+						Enter your username below to login to your account
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<form action={sumbit}>
+						<div className="flex flex-col gap-6">
+							<div className="grid gap-2">
+								<Label htmlFor="username">Username</Label>
+								<Input
+									id="username"
+									name="username"
+									type="text"
+									required
+								/>
+							</div>
+							<div className="grid gap-2">
+								<Label htmlFor="password">Password</Label>
+								<Input
+									id="password"
+									name="password"
+									type="password"
+									required />
+							</div>
+							<Button type="submit" className="w-full">
+								Login
+							</Button>
+						</div>
+					</form>
+					{error &&
+						<div className="pt-6">
+							<Alert variant="destructive">
+								<AlertTitle>Failed</AlertTitle>
+								<AlertDescription>
+									{error}
+								</AlertDescription>
+							</Alert>
+						</div>
+					}
+				</CardContent>
+			</Card>
+		</div>
+	)
+}
